a9b02785d940978ff3d3b15c3ee235e913d1d019
Felix Zumstein <fzumstein@gmail.com>
2013-12-16 23:01:39 +0100

    changed vba to deal with UNC and mapped drives

--- xls_test.py
+++ xls_test.py
@@ -6,7 +6,7 @@ import numpy as np
 # Reference to the calling Excel File
 file_name = sys.argv[1]
 dir_path = os.path.dirname(os.path.abspath(__file__))
-file_path = '{0}\\{1}'.format(dir_path, file_name)
+file_path = r'{0}\{1}'.format(dir_path, file_name)
 xl = GetObject(file_path)
 
 
