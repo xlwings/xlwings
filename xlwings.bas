b99f003603ea631bc854a75fd005856038e2246d
Felix Zumstein <fzumstein@gmail.com>
2013-12-28 15:52:45 +0100

    reorganization / refactoring

--- xlwings.bas
+++ xlwings.bas
@@ -16,54 +16,49 @@ Public Function RunPython(PythonCommand As String)
     ' Version: 0.1-dev
     ' License: MIT (see LICENSE.txt for details)
     
-    Dim PYTHON_DIR As String
-    Dim PYTHONFILE_PATH As String
-    Dim LOG_FILE As String
-    Dim wbName As String
-    Dim driveCommand As String
-    Dim exitCode As Integer
-    Dim wsh As Object
-    Dim waitOnReturn As Boolean: waitOnReturn = True
-    Dim windowStyle As Integer: windowStyle = 0
+    Dim PYTHON_DIR As String, SOURCECODE_DIR As String, LOG_FILE As String, WORKBOOK_FULL_NAME As String
+    Dim DriveCommand As String
+    Dim ExitCode As Integer
+    Dim Wsh As Object
+    Dim WaitOnReturn As Boolean: WaitOnReturn = True
+    Dim WindowStyle As Integer: WindowStyle = 0
     
-    ' Adjust according to the desired Python installation directory, e.g.: "C:\Python27"
-    ' Leave as empty string if you want to use the default installation from your PATH environment variable,
-    ' i.e. the installation you can start by just typing "python" at the command prompt
-    
+    ' Adjust according to where python.exe is on your system, e.g.: "C:\Python27"
+    ' Leave empty if you want to use the default installation from your PATH environment variable,
+    ' i.e. you want to use the installation you can start by just typing "python" at the command prompt
     PYTHON_DIR = ""
     
-    ' Adjust according to the directory of the Python file
-    PYTHONFILE_PATH = ThisWorkbook.Path
+    ' Adjust according to the directory of the Python files
+    SOURCECODE_DIR = ThisWorkbook.Path
     
-    ' Log File (fully qualified name)
+    ' Log file as fully qualified name
     LOG_FILE = ThisWorkbook.Path & "\" & "log.txt"
     
-    ' Get Workbook name
-    wbName = ThisWorkbook.Name
+    ' Get fully qualified name of Workbook
+    WORKBOOK_FULL_NAME = ThisWorkbook.FullName
     
     ' Call a command window and change to the directory of the Python installation
     ' Note: If Python is called from a different directory with the full qualified path, pywintypesXX.dll won't be found.
     ' This is likely a pywin32 bug, see http://stackoverflow.com/q/7238403/918626
     ' Run Python with the "-c" command line switch: add the path of the python file and run the
-    ' PythonCommand as first argument, then provide the wbName as second argument.
-    ' Write out the errors into "log.txt" next to this Excel file.
-    ' Wait with proceeding until the call returns.
-    Set wsh = CreateObject("WScript.Shell")
+    ' PythonCommand as first argument, then provide the WORKBOOK_FULL_NAME as second argument.
+    ' Write out the errors to the LOG_FILE and wait with proceeding until the call returns.
+    Set Wsh = CreateObject("WScript.Shell")
     If Left$(PYTHON_DIR, 2) Like "[A-Z]:" Then
         ' If Python is installed on a mapped or local drive, change to drive, then cd to path
-        driveCommand = Left$(PYTHON_DIR, 2) & " & cd " & PYTHON_DIR & " & "
+        DriveCommand = Left$(PYTHON_DIR, 2) & " & cd " & PYTHON_DIR & " & "
     ElseIf Left$(PYTHON_DIR, 2) = "\\" Then
         ' In the unlikely event that Python is installed on a UNC path, temporarily mount and activate a drive letter with pushd
-        driveCommand = "pushd " & PYTHON_DIR & " & "
+        DriveCommand = "pushd " & PYTHON_DIR & " & "
     End If
     
-    exitCode = wsh.Run("cmd.exe /C " & driveCommand & _
-                       "python -c " & """import sys; sys.path.append(r'" & PYTHONFILE_PATH & "'); " & PythonCommand & _
-                        """ """ & wbName & """ 2> """ & LOG_FILE & """  ", _
-                       windowStyle, waitOnReturn)
+    ExitCode = Wsh.Run("cmd.exe /C " & DriveCommand & _
+                       "python -c " & """import sys; sys.path.append(r'" & SOURCECODE_DIR & "'); " & PythonCommand & _
+                        """ """ & WORKBOOK_FULL_NAME & """ 2> """ & LOG_FILE & """  ", _
+                       WindowStyle, WaitOnReturn)
 
-    'If exitCode <> 0 then there's something wrong
-    If exitCode <> 0 Then
+    'If ExitCode <> 0 then there's something wrong
+    If ExitCode <> 0 Then
         Call ShowError(LOG_FILE)
         ' Delete file after the error message has been shown
         On Error Resume Next
@@ -72,11 +67,11 @@ Public Function RunPython(PythonCommand As String)
     End If
     
     ' Make sure wsh is cleared as moving the file could create troubles otherwise
-    Set wsh = Nothing
+    Set Wsh = Nothing
     
 End Function
 
-Sub ShowError(Filename As String)
+Sub ShowError(FileName As String)
     Dim ReadData As String
     Dim Token As String
     Dim FileNum As Integer
@@ -85,7 +80,7 @@ Sub ShowError(Filename As String)
     ReadData = ""
     
     ' Read Text File
-    Open Filename For Input As #FileNum
+    Open FileName For Input As #FileNum
         Do While Not EOF(FileNum)
             Line Input #FileNum, Token
             ReadData = ReadData & Token & vbCr
@@ -97,5 +92,4 @@ Sub ShowError(Filename As String)
     
     ' MsgBox
     MsgBox ReadData, vbCritical, "Error"
-    
 End Sub
--- xlwings.py
+++ xlwings.py
@@ -1,13 +1,11 @@
 """
-xlwings is an easy way to connect your Excel tools with Python (Windows only).
-The aim is to make it as easy as possible to distribute the Excel files.
-
+xlwings makes it easy to deploy your Python powered Excel tools on Windows.
 Homepage and documentation: http://xlwings.org/
 
 Copyright (c) 2013, Felix Zumstein.
 License: MIT (see LICENSE.txt for details)
 
-This module is largely based on the EasyExcel class as described in the book
+This module partly based on the easyExcel class as shown in the book
 "Python Programming on Win32". It can be downloaded from
 http://starship.python.net/crew/mhammond/ppw32
 Copyright (c) 2000, Mark Hammond and Andy Robinson
@@ -15,14 +13,16 @@ Copyright (c) 2000, Mark Hammond and Andy Robinson
 
 import sys
 import os
-import inspect
 from win32com.client import GetObject
 import adodbapi
-from pywintypes import UnicodeType, TimeType
+from pywintypes import TimeType
+import numpy as np
 from pandas import MultiIndex
 
 __version__ = '0.1-dev'
 __license__ = 'MIT'
+
+_is_python3 = sys.version_info.major > 2
 
 class Xl:
     """
@@ -32,21 +32,20 @@ class Xl:
     ----------
     filepath : string, default None
         For debugging/interactive use from within Python, provide the full filepath. Leave empty if called from Excel.
+
     """
 
-    def __init__(self, filepath=None):
-        if filepath:
+    def __init__(self, fullname=None):
+        if fullname:
             # GetObject() gives us the correct Excel instance if there are > 1
-            self.Workbook = GetObject(filepath)
-            self.filepath = filepath
+            self.Workbook = GetObject(fullname)
+            self.fullname = fullname
+            self.filename = os.path.split(self.fullname)[1]
         else:
-            # TODO: catch AttributeError in case called from Python without filepath
-            filename = sys.argv[1]
-            # Get filepath of calling function in case this module is somewhere else
-            # TODO: currently, this requires that the excel and python file are in the same directory
-            _dirpath = os.path.dirname(inspect.getmodule(inspect.stack()[1][0]).__file__)
-            self.filepath = os.path.abspath(os.path.join(_dirpath, filename))
-            self.Workbook = GetObject(self.filepath)
+            # TODO: catch AttributeError in case called from Python without argument
+            self.fullname = sys.argv[1]
+            self.Workbook = GetObject(self.fullname)
+            self.filename = os.path.split(self.fullname)[1]
 
     def save(self, newfilename=None):
         if newfilename:
@@ -56,39 +55,71 @@ class Xl:
             self.Workbook.Save()
         
     def get_cell(self, sheet, row, col):
-        """Get value of one cell"""
+        """ Get value of one cell """
         sht = self.Workbook.Worksheets(sheet)
         cell = sht.Cells(row, col).Value
         if type(cell) is TimeType:
-            return self.to_datetime([[cell]])[0][0]
+            return self.clean_data([[cell]])[0][0]
         return cell
 
-    def set_cell(self, sheet, row, col, value):
-        """set value of one cell"""
-        sht = self.Workbook.Worksheets(sheet)
-        sht.Cells(row, col).Value = value
-
     def get_range(self, sheet, row1, col1, row2, col2):
-        """returns a list of lists"""
+        """ Returns a list of lists """
         if row1 == row2 and col1 == col2:
             return self.get_cell(sheet, row1, row1)
         else:
             sht = self.Workbook.Worksheets(sheet)
             data = sht.Range(sht.Cells(row1, col1), sht.Cells(row2, col2)).Value
-            # Turn from tuple of tuples into list of lists and transform dates
-            data = [list(row) for row in data]
-            return self.to_datetime(data)
-        
+            return self.clean_data(data)
+
+    def get_contiguous_range(self, sheet, row, col):
+        # TODO: shortcut/option to ignore "" cells with xlup/xlright or CurrentRegion
+        """
+        Tracks down and across from top left cell until it
+        encounters blank cells; returns the non-blank range.
+        Looks at first row and column; blanks at bottom or right
+        are OK and return None within the array
+
+        """
+        sht = self.Workbook.Worksheets(sheet)
+
+        # find the bottom row
+        bottom = row
+        while sht.Cells(bottom + 1, col).Value not in [None, ""]:
+            bottom += 1
+
+        # right column
+        right = col
+        while sht.Cells(row, right + 1).Value not in [None, ""]:
+            right += 1
+
+        data = sht.Range(sht.Cells(row, col), sht.Cells(bottom, right)).Value
+        return self.clean_data(data)
+
+    def get_current_range(self, sheet, row, col):
+        """
+        Equivalent to CurrentRange in Excel: Takes all surrounding cells into account
+
+        """
+        data = self.Workbook.Worksheets(sheet).Cells(row, col).CurrentRegion.Value
+        data = [list(row) for row in data]
+        return self.clean_data(data)
+
+    def set_cell(self, sheet, row, col, value):
+        """ Set value of one cell """
+        sht = self.Workbook.Worksheets(sheet)
+        sht.Cells(row, col).Value = value
+
     def set_range(self, sheet, top_row, left_col, data):
         """
         Insert a 2d array starting at given location.
         Works out the size needed for itself
         
         """
-    
         bottom_row = top_row + len(data) - 1
         right_col = left_col + len(data[0]) - 1
         sht = self.Workbook.Worksheets(sheet)
+        if type(data) is np.ndarray:
+            data = data.tolist()  # Python 3 cant handle arrays directly
         sht.Range(sht.Cells(top_row, left_col), sht.Cells(bottom_row, right_col)).Value = data
 
     def set_dataframe(self, sheet, top_row, left_col, dataframe, index=True, header=True):
@@ -113,61 +144,34 @@ class Xl:
 
         self.set_range(sheet, top_row, left_col, dataframe.values)
 
-
-    def get_contiguous_range(self, sheet, row, col):
-        # TODO: shortcut/option to ignore "" cells with xlup/xlright or CurrentRegion
-        # TODO: don't restrict to first col/row
-        """
-        Tracks down and across from top left cell until it
-        encounters blank cells; returns the non-blank range.
-        Looks at first row and column; blanks at bottom or right
-        are OK and return None within the array
-
-        """
-    
-        sht = self.Workbook.Worksheets(sheet)
-    
-        # find the bottom row
-        bottom = row
-        while sht.Cells(bottom + 1, col).Value not in [None, ""]:
-            bottom += 1
-    
-        # right column
-        right = col
-        while sht.Cells(row, right + 1).Value not in [None, ""]:
-            right += 1
-    
-        return list(sht.Range(sht.Cells(row, col), sht.Cells(bottom, right)).Value)
-
-    def get_current_range(self, sheet, row, col):
-        """
-        Equivalent to CurrentRange in Excel: Takes all surrounding cells into account
-        """
-        data = self.Workbook.Worksheets(sheet).Cells(row, col).CurrentRegion.Value
-        data = [list(row) for row in data]
-        return data
-
     @staticmethod
-    def to_datetime(data):
+    def clean_data(data):
         """
-        Transforms PyTime Objects from COM into datetime objects
-        TODO: simplify
+        Transforms data from tuples of tuples into list of list and
+        on Python 2, transforms PyTime Objects from COM into datetime objects.
 
         Parameters
         ----------
-        data : list of list
+        data : raw data as returned from Excel (tuple of tuple)
 
         """
-        # Check which columns contain COM dates
-        cols_with_dates = []
-        for i in range(len(data[0])):
-            if any([type(row[i]) is TimeType for row in data]):
-                cols_with_dates.append(i)
+        # turn into list of list for easier handling (e.g. for Pandas DataFrame)
+        data = [list(row) for row in data]
 
-        # Transform PyTime into datetime
-        tc = adodbapi.pythonDateTimeConverter()
-        for i in cols_with_dates:
-            for j, cell in enumerate([row[i] for row in data]):
-                if type(cell) is TimeType:
-                    data[j][i] = tc.DateObjectFromCOMDate(cell)
-        return data
+        # Check which columns contain COM dates
+        # TODO: simplify
+        if _is_python3 is True:
+            return data
+        else:
+            cols_with_dates = []
+            for i in range(len(data[0])):
+                if any([type(row[i]) is TimeType for row in data]):
+                    cols_with_dates.append(i)
+
+            # Transform PyTime into datetime
+            tc = adodbapi.pythonDateTimeConverter()
+            for i in cols_with_dates:
+                for j, cell in enumerate([row[i] for row in data]):
+                    if type(cell) is TimeType:
+                        data[j][i] = tc.DateObjectFromCOMDate(cell)
+            return data
