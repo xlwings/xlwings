430ef1916a400476d5ae0ec41e98f0cd6275a5a1
Felix Zumstein <fzumstein@gmail.com>
2014-01-19 14:50:29 +0100

    switched to nose

--- tests/test_range.py
+++ tests/test_range.py
@@ -1,175 +1,49 @@
 # -*- coding: utf-8 -*-
-import sys
 import os
-import unittest
+import nose
+from nose.tools import assert_equal
 from datetime import datetime
 
-sys.path.append('..')
 from xlwings import xlwings_connect, Range
 
 xl_file1 = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test1.xlsx')
 wb = xlwings_connect(xl_file1)
 
 
-class TestRange(unittest.TestCase):
-
-    # Syntax: Range('A1')
-    def test_activesheet_celladdress_integer(self):
-        value = 22
-        Range('A1').value = value
-        cell = Range('A1').value
-        self.assertEqual(cell, value)
-
-    def test_activesheet_celladdress_float(self):
-        value = 22.2222
-        Range('A1').value = value
-        cell = Range('A1').value
-        self.assertEqual(cell, value)
-
-    def test_activesheet_celladdress_string(self):
-        value = 'Test String'
-        Range('A1').value = value
-        cell = Range('A1').value
-        self.assertEqual(cell, value)
-
-    def test_activesheet_celladdress_unicode(self):
-        value = u'éöà'
-        Range('A1').value = value
-        cell = Range('A1').value
-        self.assertEqual(cell, value)
-
-    def test_activesheet_celladdress_date(self):
-        value = datetime(1962, 11, 3)
-        Range('A2').value = value
-        cell = Range('A2').value
-        self.assertEqual(cell, value)
+def test_cell():
+    """ Style: Range('A1') """
+    params = [('A1', 22),
+              ((1,1), 22),
+              ('A1', 22.2222),
+              ((1,1), 22.2222),
+              ('A1', 'Test String'),
+              ((1,1), 'Test String'),
+              ('A1', u'éöà'),
+              ((1,1), u'éöà'),
+              ('A2', datetime(1962, 11, 3)),
+              ((2,1), datetime(1962, 11, 3)),
+              ('A3', datetime(2020, 12, 31, 12, 12, 20)),
+              ((3,1), datetime(2020, 12, 31, 12, 12, 20))]
+    for param in params:
+        yield check_cell, param[0], param[1]
 
 
-    # Syntax: Range('SheetName', 'A1')
-    def test_sheetname_celladdress_integer(self):
-        value = 22
-        Range('Sheet2', 'A1').value = value
-        cell = Range('Sheet2', 'A1').value
-        self.assertEqual(cell, value)
+def check_cell(address, value):
+        # Active Sheet
+        Range(address).value = value
+        cell = Range(address).value
+        assert_equal(cell, value)
 
-    def test_sheetname_celladdress_float(self):
-        value = 22.2222
-        Range('Sheet2', 'A1').value = value
-        cell = Range('Sheet2', 'A1').value
-        self.assertEqual(cell, value)
+        # Sheetname
+        Range('Sheet2', address).value = value
+        cell = Range('Sheet2', address).value
+        assert_equal(cell, value)
 
-    def test_sheetname_celladdress_string(self):
-        value = 'Test String'
-        Range('Sheet2', 'A1').value = value
-        cell = Range('Sheet2', 'A1').value
-        self.assertEqual(cell, value)
-
-    def test_sheetname_celladdress_unicode(self):
-        value = u'éöà'
-        Range('Sheet2', 'A1').value = value
-        cell = Range('Sheet2', 'A1').value
-        self.assertEqual(cell, value)
-
-    # Syntax: Range(1, 'A1')
-    def test_sheetindex_celladdress_integer(self):
-        value = 22
-        Range(3, 'A1').value = value
-        cell = Range(3, 'A1').value
-        self.assertEqual(cell, value)
-
-    def test_sheetindex_celladdress_float(self):
-        value = 22.2222
-        Range(3, 'A1').value = value
-        cell = Range(3, 'A1').value
-        self.assertEqual(cell, value)
-
-    def test_sheetindex_celladdress_string(self):
-        value = 'Test String'
-        Range(3, 'A1').value = value
-        cell = Range(3, 'A1').value
-        self.assertEqual(cell, value)
-
-    def test_sheetindex_celladdress_unicode(self):
-        value = u'éöà'
-        Range(3, 'A1').value = value
-        cell = Range(3, 'A1').value
-        self.assertEqual(cell, value)
-
-    # Syntax: Range((1,1))
-    def test_activesheet_cellindex_integer(self):
-        value = 22
-        Range((1,1)).value = value
-        cell = Range((1,1)).value
-        self.assertEqual(cell, value)
-
-    def test_activesheet_cellindex_float(self):
-        value = 22.2222
-        Range((1,1)).value = value
-        cell = Range((1,1)).value
-        self.assertEqual(cell, value)
-
-    def test_activesheet_cell_cellindex_string(self):
-        value = 'Test String'
-        Range((1,1)).value = value
-        cell = Range((1,1)).value
-        self.assertEqual(cell, value)
-
-    def test_activesheet_cellindex_unicode(self):
-        value = u'éöà'
-        Range((1,1)).value = value
-        cell = Range((1,1)).value
-        self.assertEqual(cell, value)
-
-    # Syntax: Range('SheetName', (1,1))
-    def test_sheetname_cellindex_integer(self):
-        value = 22
-        Range('Sheet2', (1,1)).value = value
-        cell = Range('Sheet2', (1,1)).value
-        self.assertEqual(cell, value)
-
-    def test_sheetname_cellindex_float(self):
-        value = 22.2222
-        Range('Sheet2', (1,1)).value = value
-        cell = Range('Sheet2', (1,1)).value
-        self.assertEqual(cell, value)
-
-    def test_sheetname_cellindex_string(self):
-        value = 'Test String'
-        Range('Sheet2', (1,1)).value = value
-        cell = Range('Sheet2', (1,1)).value
-        self.assertEqual(cell, value)
-
-    def test_sheetname_cellindex_unicode(self):
-        value = u'éöà'
-        Range('Sheet2', (1,1)).value = value
-        cell = Range('Sheet2', (1,1)).value
-        self.assertEqual(cell, value)
-
-    # Syntax: Range(1, (1,1))
-    def test_sheetindex_cellindex_integer(self):
-        value = 22
-        Range(3, (1,1)).value = value
-        cell = Range(3, (1,1)).value
-        self.assertEqual(cell, value)
-
-    def test_sheetindex_cellindex_float(self):
-        value = 22.2222
-        Range(3, (1,1)).value = value
-        cell = Range(3, (1,1)).value
-        self.assertEqual(cell, value)
-
-    def test_sheetindex_cellindex_string(self):
-        value = 'Test String'
-        Range(3, (1,1)).value = value
-        cell = Range(3, (1,1)).value
-        self.assertEqual(cell, value)
-
-    def test_sheetindex_cellindex_unicode(self):
-        value = u'éöà'
-        Range(3, (1,1)).value = value
-        cell = Range(3, (1,1)).value
-        self.assertEqual(cell, value)
+        # Sheetindex
+        Range(3, address).value = value
+        cell = Range(3, address).value
+        assert_equal(cell, value)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    nose.main()
