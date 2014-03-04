c7990a58d44e73bc70ce6fdd489464d7d94b471b
Felix Zumstein <fzumstein@gmail.com>
2014-03-04 15:48:49 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='xlwings',
+    version='0.1.0',
+    packages=['xlwings'],
+    url='http://xlwings.org',
+    license='BSD 3-clause',
+    author='Zoomer Analytics LLC',
+    author_email='felix.zumstein@zoomeranalytics.com',
+    description='Make Excel fly!'
+)
