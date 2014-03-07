ceeac51d0729e697dd908619ace5647942734939
Felix Zumstein <fzumstein@gmail.com>
2014-03-07 15:00:51 +0100

    fiddled around with setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='xlwings',
     version='0.1.0',
-    packages=['xlwings'],
     url='http://xlwings.org',
     license='BSD 3-clause',
     author='Zoomer Analytics LLC',
     author_email='felix.zumstein@zoomeranalytics.com',
-    description='Make Excel fly!'
+    description='Make Excel fly: Interact with Excel from Python and vice versa.',
+    long_description='TODO',
+    packages=['xlwings'],
+    package_data={'xlwings': ['examples/*.*']},
+    # include_package_data=True,
+    platforms='Win32 (MS Windows)',
+    keywords=['xls', 'excel', 'spreadsheet', 'workbook'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Win32 (MS Windows)',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+        'License :: OSI Approved :: BSD License']
 )
