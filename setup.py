fd180b880bb725a43b03cc6ecf1cab8597becfaf
Felix Zumstein <fzumstein@gmail.com>
2014-03-09 20:07:46 +0100

    automatic long_description and version

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
+import os
+import re
 from distutils.core import setup
+
+# long_description: Take from README file
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    readme = f.read()
+
+# Version Number
+with open(os.path.join(os.path.dirname(__file__), 'xlwings', 'xlwings.py')) as f:
+    version = re.compile(r".*__version__ = '(.*?)'", re.S).match(f.read()).group(1)
 
 setup(
     name='xlwings',
-    version='0.1.0',
+    version=version,
     url='http://xlwings.org',
     license='BSD 3-clause',
     author='Zoomer Analytics LLC',
     author_email='felix.zumstein@zoomeranalytics.com',
     description='Make Excel fly: Interact with Excel from Python and vice versa.',
-    long_description='TODO',
+    long_description=readme,
     packages=['xlwings'],
     package_data={'xlwings': ['*.bas',
+                              'tests/*.*',
                               'examples/*.*']},
-    platforms='Win32 (MS Windows)',
+    platforms='Operating System :: Microsoft :: Windows',
     keywords=['xls', 'excel', 'spreadsheet', 'workbook'],
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Environment :: Win32 (MS Windows)',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
