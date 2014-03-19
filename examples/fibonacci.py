6462670e73ba36f90b4bcd575ea23d4d0c7924bb
Felix Zumstein <fzumstein@gmail.com>
2014-03-19 10:37:58 +0100

    some refactoring on the fibonacci example

--- examples/fibonacci.py
+++ examples/fibonacci.py
@@ -2,20 +2,29 @@ from xlwings import Workbook, Range
 
 wb = Workbook()  # Create a reference to the calling Excel Workbook
 
-def fibonacci():
+
+def fibonacci(n):
     """
     Generates the first n Fibonacci numbers.
+    """
+    seq = [1, 1]
+    for i in range(1, n-1):
+        seq.append(seq[i-1] + seq[i])
+    return seq[:n]
+
+
+def xl_fibonacci():
+    """
+    This is a wrapper around fibonacci() to handle all the Excel stuff
     """
     # Get the input from Excel and turn into integer
     n = int(Range('B1').value)
 
-    # Calculate the Fibonacci Sequence
-    seq = [1, 1]
-    for i in range(1, n):
-        seq.append(seq[i-1] + seq[i])
+    # Call the main function
+    seq = fibonacci(n)
 
     # Clear output
     Range('C1').vertical.clear_contents()
 
     # Return the output to Excel - use zip to get column format
-    Range('C1').value = zip(seq[:n])
+    Range('C1').value = zip(seq)
