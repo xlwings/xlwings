da451b5620fa49cf5c5b039b097e09b868b47128
Felix Zumstein <fzumstein@gmail.com>
2014-06-20 12:38:06 +0200

    added color and version infos for python, numpy, pandas

--- runtests.py
+++ runtests.py
@@ -2,6 +2,7 @@
 # I had to fall back to clean full installations for now.
 from __future__ import print_function
 import os
+import sys
 from subprocess import call
 from xlwings import __version__
 
@@ -12,6 +13,11 @@ py31 = r'C:\Python31'  # numpy/pandas are missing for 3.1
 py32 = r'C:\Python32'
 py33 = r'C:\Python33'
 py34 = r'C:\Python34'
+
+
+class colors:
+    yellow = '\033[93m'
+    end = '\033[0m'
 
 this_path = os.path.dirname(__file__)
 setup_file = os.path.abspath(os.path.join(this_path, 'setup.py'))
@@ -27,7 +33,11 @@ for py in [py26, py27, py31, py32, py33, py34]:
     test_dir = os.path.abspath(os.path.join(py, 'Lib/site-packages/xlwings/tests'))
     xlwings_package = os.path.abspath(os.path.join(this_path, 'dist/xlwings-{}.zip'.format(__version__)))
 
-    print('### {} ###'.format(py))
+    print('{}### {} ###{}'.format(colors.yellow, py, colors.end))
+
+    call('{}\python -c "import sys;print(sys.version)"'.format(py))
+    call('{}\python -c "import numpy;import pandas;print(numpy.__version__);print(pandas.__version__)"'.format(py))
+
 
     # Uninstall in case there is still an existing installation
     call('{} uninstall xlwings -y'.format(pip))
