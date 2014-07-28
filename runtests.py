bc18f2e68c68a4a7e9ffe6918e7dc8a2f5f624bd
Felix Zumstein <fzumstein@gmail.com>
2014-07-28 20:52:44 +0200

    fixed this_dir issue

--- runtests.py
+++ runtests.py
@@ -1,7 +1,10 @@
-# Since I couldn't make tox or conda environments work for all Python versions including pywin32/numpy/pandas,
-# I had to fall back to clean full installations for now.
+# Run this file on Windows only. It expects full Python installations with all dependencies in the paths below.
+# On Mac, run the tests with tox.
+
+
 from __future__ import print_function
 import os
+import inspect
 from subprocess import call
 from xlwings import __version__
 
@@ -14,12 +17,13 @@ py33 = r'C:\Python33'
 py34 = r'C:\Python34'
 
 
-class colors:
+class Colors:
+    # Only has an effect in the Terminal of PyCharm
     yellow = '\033[93m'
     end = '\033[0m'
 
-this_path = os.path.dirname(__file__)
-setup_file = os.path.abspath(os.path.join(this_path, 'setup.py'))
+this_dir = os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe())))
+setup_file = os.path.abspath(os.path.join(this_dir, 'setup.py'))
 
 # Create distribution package
 call('python {0} sdist'.format(setup_file))
@@ -30,9 +34,9 @@ for py in [py26, py27, py31, py32, py33, py34]:
     pip = os.path.abspath(os.path.join(py, 'Scripts/pip'))
     test_runner = os.path.abspath(os.path.join(py, 'Scripts/nosetests'))
     test_dir = os.path.abspath(os.path.join(py, 'Lib/site-packages/xlwings/tests'))
-    xlwings_package = os.path.abspath(os.path.join(this_path, 'dist/xlwings-{0}.zip'.format(__version__)))
+    xlwings_package = os.path.abspath(os.path.join(this_dir, 'dist/xlwings-{0}.zip'.format(__version__)))
 
-    print('{0}### {1} ###{2}'.format(colors.yellow, py, colors.end))
+    print('{0}### {1} ###{2}'.format(Colors.yellow, py, Colors.end))
 
     call('{0}\python -c "import sys;print(sys.version)"'.format(py))
     call('{0}\python -c "import numpy;import pandas;print(numpy.__version__);print(pandas.__version__)"'.format(py))
