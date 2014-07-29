b5298ba90768af28fc675579db1b14230261a17f
Felix Zumstein <fzumstein@gmail.com>
2014-07-29 16:31:17 +0200

    upgraded for xlwings v0.2.0

--- examples/fibonacci/build_lite.py
+++ examples/fibonacci/build_lite.py
@@ -5,12 +5,12 @@ from subprocess import call
 def main():
     # Zip it up - 7-zip provides better compression than the zipfile module
     # Make sure the 7-zip folder is on your path
-    file_name = 'simulation'
+    file_name = 'fibonacci'
     if os.path.isfile('{}.zip'.format(file_name)):
         os.remove('{}.zip'.format(file_name))
     call('7z a -tzip {}.zip {}.xlsm'.format(file_name, file_name))
     call('7z a -tzip {}.zip LICENSE.txt'.format(file_name))
-    call('7z a -tzip {}.zip simulation.py'.format(file_name))
+    call('7z a -tzip {}.zip fibonacci.py'.format(file_name))
 
 
 if __name__ == '__main__':
