9187b59c4b0296eddd1b3415debe64128018570e
Felix Zumstein <fzumstein@gmail.com>
2015-01-16 17:36:31 +0100

    updated samples for v0.3.1

--- examples/database/database.py
+++ examples/database/database.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2014, Zoomer Analytics LLC.
+Copyright (C) 2014-2015, Zoomer Analytics LLC.
 All rights reserved.
 
 License: BSD 3-clause (see LICENSE.txt for details)
@@ -17,7 +17,7 @@ def playlist():
     wb = Workbook.caller()
 
     # Place the database next to the Excel file
-    db_file = os.path.join(os.path.dirname(wb.fullname), 'chinook.sqlite')
+    db_file = os.path.join(os.path.dirname(__file__), 'chinook.sqlite')
 
     # Database connection and creation of cursor
     con = sqlite3.connect(db_file, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
@@ -64,7 +64,7 @@ def combobox():
     wb = Workbook.caller()
 
     # Place the database next to the Excel file
-    db_file = os.path.join(os.path.dirname(wb.fullname), 'chinook.sqlite')
+    db_file = os.path.join(os.path.dirname(__file__), 'chinook.sqlite')
 
     # Database connection and creation of cursor
     con = sqlite3.connect(db_file, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
