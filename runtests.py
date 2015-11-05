723ae8696a7c6f5f293a68d1748eb76d2bb22532
sdementen <sebitt123>
2015-11-05 08:52:06 +0100

    add "" to take care of cases where there are blank spaces in the path

--- runtests.py
+++ runtests.py
@@ -26,10 +26,14 @@ this_dir = os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe(
 setup_file = os.path.abspath(os.path.join(this_dir, 'setup.py'))
 
 # Create distribution package
-call('python {0} sdist'.format(setup_file))
+call('python "{0}" sdist'.format(setup_file))
 
 # Install it, run the tests and uninstall it for each Python version
 for py in [py26, py27, py31, py32, py33, py34]:
+    # skip testing is python interpreter path cannot be found
+    if not os.path.exists(py):
+        continue
+
     # Paths
     pip = os.path.abspath(os.path.join(py, 'Scripts/pip'))
     test_runner = os.path.abspath(os.path.join(py, 'Scripts/nosetests'))
@@ -38,18 +42,18 @@ for py in [py26, py27, py31, py32, py33, py34]:
 
     print('{0}### {1} ###{2}'.format(Colors.yellow, py, Colors.end))
 
-    call('{0}\python -c "import sys;print(sys.version)"'.format(py))
-    call('{0}\python -c "import numpy;import pandas;print(numpy.__version__);print(pandas.__version__)"'.format(py))
+    call('"{0}\python" -c "import sys;print(sys.version)"'.format(py))
+    call('"{0}\python" -c "import numpy;import pandas;print(numpy.__version__);print(pandas.__version__)"'.format(py))
 
     # Uninstall in case there is still an existing installation
-    call('{0} uninstall xlwings -y'.format(pip))
+    call('"{0}" uninstall xlwings -y'.format(pip))
 
     # Install
     os.chdir(py)
-    call('{0} install {1}'.format(pip, xlwings_package))
+    call('"{0}" install "{1}"'.format(pip, xlwings_package))
 
     # Run tests
-    call('{0} {1}'.format(test_runner, test_dir))
+    call('"{0}" "{1}"'.format(test_runner, test_dir))
 
     # Uninstall
-    call('{0} uninstall xlwings -y'.format(pip))
+    call('"{0}" uninstall xlwings -y'.format(pip))
