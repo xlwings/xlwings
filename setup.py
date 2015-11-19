f0fae2d4b3488afe8781c68274440bf9fee6964d
Felix Zumstein <fzumstein@gmail.com>
2015-11-19 15:20:44 +0100

    don't include xlwings.xlam.bas file when installing the package

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     long_description=readme,
     data_files=data_files,
     packages=['xlwings', 'xlwings.tests'],
-    package_data={'xlwings': ['*.bas', 'tests/*.xlsx', 'tests/*.png', 'xlwings_template.xltm', 'xlwings.xlam']},
+    package_data={'xlwings': ['xlwings.bas', 'tests/*.xlsx', 'tests/*.png', 'xlwings_template.xltm', 'xlwings.xlam']},
     keywords=['xls', 'excel', 'spreadsheet', 'workbook', 'vba', 'macro'],
     install_requires=install_requires,
     classifiers=[
