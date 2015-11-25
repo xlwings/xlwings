6457fe124e2f2ee7470933a16e85570150fd2ec1
Felix Zumstein <fzumstein@gmail.com>
2015-11-25 18:07:25 +0100

    added udf sample

---
+++ examples/udf/build_lite.py
@@ -0,0 +1,17 @@
+import os
+from subprocess import call
+
+
+def main():
+    # Zip it up - 7-zip provides better compression than the zipfile module
+    # Make sure the 7-zip folder is on your path
+    file_name = 'udf'
+    if os.path.isfile('{}.zip'.format(file_name)):
+        os.remove('{}.zip'.format(file_name))
+    call('7z a -tzip {}.zip {}.xlsm'.format(file_name, file_name))
+    call('7z a -tzip {}.zip LICENSE.txt'.format(file_name))
+    call('7z a -tzip {}.zip udf.py'.format(file_name))
+
+
+if __name__ == '__main__':
+   main()
