e030b97ca967789951a8496fe53738993a1c088e
Felix Zumstein <fzumstein@gmail.com>
2015-12-04 12:02:37 +0000

    Fix ZombieProcess error caused by psutil>= 3.0.0, fixes #315 and fixes #320

--- xlwings/_xlmac.py
+++ xlwings/_xlmac.py
@@ -75,28 +75,34 @@ def is_file_open(fullname):
     Checks if the file is already open
     """
     for proc in psutil.process_iter():
-        if proc.name() == 'Microsoft Excel':
-            for i in proc.open_files():
-                path = i.path
-                if PY3:
-                    if path.lower() == fullname.lower():
-                        return True
-                else:
-                    if isinstance(path, str):
-                        path = unicode(path, 'utf-8')
-                        # Mac saves unicode data in decomposed form, e.g. an e with accent is stored as 2 code points
-                        path = unicodedata.normalize('NFKC', path)
-                    if isinstance(fullname, str):
-                        fullname = unicode(fullname, 'utf-8')
-                    if path.lower() == fullname.lower():
-                        return True
+        try:
+            if proc.name() == 'Microsoft Excel':
+                for i in proc.open_files():
+                    path = i.path
+                    if PY3:
+                        if path.lower() == fullname.lower():
+                            return True
+                    else:
+                        if isinstance(path, str):
+                            path = unicode(path, 'utf-8')
+                            # Mac saves unicode data in decomposed form, e.g. an e with accent is stored as 2 code points
+                            path = unicodedata.normalize('NFKC', path)
+                        if isinstance(fullname, str):
+                            fullname = unicode(fullname, 'utf-8')
+                        if path.lower() == fullname.lower():
+                            return True
+        except psutil.NoSuchProcess:
+            pass
     return False
 
 
 def is_excel_running():
     for proc in psutil.process_iter():
-        if proc.name() == 'Microsoft Excel':
-            return True
+        try:
+            if proc.name() == 'Microsoft Excel':
+                return True
+        except psutil.NoSuchProcess:
+            pass
     return False
 
 
