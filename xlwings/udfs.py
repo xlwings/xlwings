6943fa7d3e71cadd857318373c18fe581775e57b
Felix Zumstein <fzumstein@gmail.com>
2016-01-06 10:09:55 +0100

    also set Workbook.caller() for xlfunc, closes #316

--- xlwings/udfs.py
+++ xlwings/udfs.py
@@ -201,6 +201,7 @@ def import_udfs(script_path, xl_workbook):
                 f.write(tab + 'Py.SetAttr Py.Module("xlwings._xlwindows"), "xl_workbook_current", ThisWorkbook\n')
                 f.write(tab + "Py.Call func, args\n")
             else:
+                f.write(tab + 'Py.SetAttr Py.Module("xlwings._xlwindows"), "xl_workbook_current", ThisWorkbook\n')
                 f.write(tab + "Set " + fname + " = Py.Call(func, args)\n")
                 marshal = xlret["marshal"]
                 if marshal == "auto":
