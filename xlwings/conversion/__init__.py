90d23c6905a218c74e3e6fb698900e2e559caa7d
Eric Reynolds <eric.remo.reynolds@gmail.com>
2016-01-28 09:46:42 +0100

    First experimental refactor of accessors code

--- xlwings/conversion/__init__.py
+++ xlwings/conversion/__init__.py
@@ -4,8 +4,6 @@ from .. import xlplatform
 
 from ..main import Range
 
-from ..utils import WithOverrides
-
 import datetime
 
 
@@ -30,6 +28,131 @@ _date_handlers = {
 }
 
 
+class Options(dict):
+
+    def __init__(self, original):
+        super(Options, self).__init__(original)
+
+    def override(self, **overrides):
+        self.update(overrides)
+        return self
+
+    def erase(self, keys):
+        for key in keys:
+            self.pop(key, None)
+        return self
+
+    def defaults(self, **defaults):
+        for k, v in defaults:
+            if k not in self:
+                self[k] = v
+        return self
+
+
+class ResizeRange(object):
+    def __init__(self, options):
+        self.expand = options.get('expand', None)
+
+    def read(self, value):
+        if isinstance(value, Range):
+            # auto-expand the range
+            if self.expand:
+                value = getattr(value, self.expand)
+            return value
+        else:
+            raise ValueError("Expected Range object")
+
+
+class ExtractValue(object):
+
+    def read(self, value):
+        value = xlplatform.get_value_from_range(value.xl_range)
+        if type(value) is not list:
+            value = [[value]]
+        return value
+
+
+
+class CleanValueData(object):
+    def __init__(self, options):
+        dates_as = options.get('dates_as', datetime.datetime)
+        self.dates_handler = _date_handlers.get(dates_as, dates_as)
+
+    def read(self, value):
+        return xlplatform.clean_value_data(value, self.dates_handler)
+
+
+
+class AdjustDimensionality(object):
+
+    def __init__(self, options):
+        self.ndim = options.get('ndim', None)
+
+
+    def read(self, value):
+
+        # the assumption is that value is 2-dimensional at this stage
+
+        if self.ndim is None:
+            if len(value) == 1:
+                return value[0][0] if len(value) == 0 else value[0]
+            elif len(value[0]) == 1:
+                return [ x[0] for x in value ]
+            else:
+                return value
+
+        elif self.ndim == 1:
+            if len(value) == 1:
+                return value[0]
+            elif len(value[0]) == 1:
+                return [ x[0] for x in value ]
+            else:
+                raise Exception("Range must be 1-by-n or n-by-1 when ndim=1.")
+
+        elif self.ndim == 2:
+            return value
+
+        else:
+            raise ValueError('Invalid value ndim=%s' % self.ndim)
+
+
+class Transpose(object):
+
+    def read(self, value):
+        return [[e[i] for e in value] for i in range(len(value[0]) if value else 0)]
+
+    def write(self, value, rng):
+        return [[e[i] for e in value] for i in range(len(value[0]) if value else 0)]
+
+
+
+class Pipeline(list):
+
+    def add_stage(self, stage, only_if=True):
+        if only_if:
+            self.append(stage)
+        return self
+
+    def insert_stage(self, stage, index=None, after=None, before=None, replace=None, only_if=True):
+        if only_if:
+            if sum(x is not None for x in (index, after, before, replace)) != 1:
+                raise ValueError("Must specify exactly one of arguments: index, after, before, replace")
+            if index is not None:
+                indices = (index,)
+            elif after is not None:
+                indices = tuple(i+1 for i, x in enumerate(self) if isinstance(x, after))
+            elif before is not None:
+                indices = tuple(i for i, x in enumerate(self) if isinstance(x, before))
+            elif replace is not None:
+                for i, x in enumerate(self):
+                    if isinstance(x, replace):
+                        self[i] = stage
+                return self
+            for i in reversed(indices):
+                self.insert(i, stage)
+        return self
+
+
 class RangeAccessor(object):
 
     types = ()
@@ -38,11 +161,33 @@ class RangeAccessor(object):
         # auto-expand the range
         expand = options.get('expand', None)
         if expand == 'vertical':
-            pass
+            with vba.block("If TypeOf {argname} Is Range", argname=argname):
+                with vba.block("If IsEmpty({argname}) Then", argname=argname):
+                    vba.writeln("{argname} = Array()", argname=argname)
+                with vba.block("ElseIf Not IsEmpty({argname}.Offset(1, 0).Value", argname=argname):
+                    vba.writeln("Set {argname} = Range({argname}, {argname}.End(xlDown))", argname=argname)
+                vba.writeln("End If")
+            vba.writeln("End If")
         elif expand == 'horizontal':
-            pass
+            with vba.block("If TypeOf {argname} Is Range", argname=argname):
+                with vba.block("If IsEmpty({argname}) Then", argname=argname):
+                    vba.writeln("{argname} = Array()", argname=argname)
+                with vba.block("ElseIf Not IsEmpty({argname}.Offset(0, 1).Value", argname=argname):
+                    vba.writeln("Set {argname} = Range({argname}, {argname}.End(xlRight))", argname=argname)
+                vba.writeln("End If")
+            vba.writeln("End If")
         elif expand == 'table':
-            pass
+            with vba.block("If TypeOf {argname} Is Range", argname=argname):
+                with vba.block("If IsEmpty({argname}) Then", argname=argname):
+                    vba.writeln("{argname} = Array()", argname=argname)
+                with vba.block("ElseIf Not IsEmpty({argname}.Offset(0, 1).Value", argname=argname):
+                    with vba.block("If Not IsEmpty({argname}.Offset(1, 0).Value", argname=argname):
+                        vba.writeln("Set {argname} = Range({argname}, {argname}.End(xlRight).End(xlDown))", argname=argname)
+                    with vba.block("Else"):
+                        vba.writeln("Set {argname} = Range({argname}, {argname}.End(xlRight))", argname=argname)
+                    vba.writeln("End If")
+                vba.writeln("End If")
+            vba.writeln("End If")
 
     def read(self, value, options):
         if isinstance(value, Range):
@@ -79,41 +224,21 @@ class ValueAccessor(RangeAccessor):
 
     types = (int, float, list, tuple, str, bool)
 
-    def _ensure_dimensionality(self, value, ndim):
+    def reader(self, options):
+        return (
+            Pipeline()
+            .add_stage(ResizeRange(options))
+            .add_stage(ExtractValue(options))
+            .add_stage(CleanValueData(options))
+            .add_stage(Transpose(), only_if=options.get('transpose', False))
+            .add_stage(AdjustDimensionality(options))
+        )
 
-        if ndim is None:
-            if isinstance(value, (list, tuple)):
-                if value and isinstance(value[0], (list, tuple)):
-                    if len(value) == 1:
-                        return value[0]
-                    elif len(value[0]) == 1:
-                        return [x[0] for x in value]
-            return value
-
-        if ndim == 1:
-            if isinstance(value, (list, tuple)):
-                if len(value) > 0 and isinstance(value[0], (list, tuple)):
-                    if len(value) == 1:
-                        return value[0]
-                    elif len(value[0]) == 1:
-                        return [x[0] for x in value]
-                    else:
-                        raise Exception("Range must be 1-by-n or n-by-1 when ndim=1.")
-                else:
-                    return value
-            else:
-                return [value]
-
-        if ndim == 2:
-            if isinstance(value, (list, tuple)):
-                if len(value) > 0 and isinstance(value[0], (list, tuple)):
-                    return value
-                else:
-                    return [value]
-            else:
-                return [[value]]
-
-        raise ValueError('Invalid value ndim=%s' % ndim)
+    def writer(self, options):
+        return (
+            .add_stage()
+            .add_stage(ResizeRange(options))
+        )
 
     def vba_read(self, vba, argname, options):
         RangeAccessor.vba_read(self, vba, argname, options)
@@ -170,7 +295,7 @@ if np:
         types = (np.ndarray,)
 
         def read(self, value, options):
-            value = ValueAccessor.read(self, value, WithOverrides(options, ndim=WithOverrides.deleted))
+            value = ValueAccessor.read(self, value, Options(options).erase('ndim'))
             if value is None:
                 value = np.nan
             elif isinstance(value, list):
@@ -207,7 +332,7 @@ if pd:
         types = (pd.DataFrame,)
 
         def read(self, value, options):
-            value = ValueAccessor.read(self, value, WithOverrides(options, ndim=2))
+            value = ValueAccessor.read(self, value, Options(options, ndim=2))
             return pd.DataFrame(value[1:], columns=value[0])
 
         def write(self, value, rng, options):
@@ -230,12 +355,12 @@ if pd:
 
     converters[pd.DataFrame] = PandasDataFrameAccessor()
 
-    class PandasSeriesAccessor(ValueAccessor):
+    class PandaSeriesStage(ValueAccessor):
 
         types = (pd.Series,)
 
         def read(self, value, options):
-            value = ValueAccessor.read(self, value, WithOverrides(options, ndim=1, expand='table'))
+            value = ValueAccessor.read(self, value, Options(options, ndim=1, expand='table'))
             return pd.Series(value[1:])
 
         def write(self, value, rng, options):
@@ -247,3 +372,65 @@ if pd:
             return ValueAccessor.write(self, value, rng, options)
 
     converters[pd.Series] = PandasSeriesAccessor()
+
+
+    class ConvertPandasDataFrame(object):
+
+        def __init__(self, options):
+            self.index = options.get('index', True)
+            self.header = options.get('header', True)
+
+        def read(self, value):
+            return pd.DataFrame(value[1:], columns=value[0])
+
+        def write(self, value, rng):
+            if self.index:
+                if value.index.name in value.columns:
+                    # Prevents column name collision when resetting the index
+                    value.index.rename(None, inplace=True)
+                value = value.reset_index()
+
+            if self.header:
+                if isinstance(value.columns, pd.MultiIndex):
+                    columns = list(zip(*value.columns.tolist()))
+                else:
+                    columns = [value.columns.tolist()]
+                value = columns + value.values.tolist()
+            else:
+                value = value.values.tolist()
+
+            return value, rng
+
+    class PandasDataFrameAccessor:
+
+        @staticmethod
+        def reader(options):
+            return (
+                converters[None].reader(
+                    Options(options)
+                    .override(ndim=2)
+                    .defaults(expand='table')
+                )
+                .add_stage(ConvertPandasDataFrame(options))
+            )
+
+        @staticmethod
+        def writer(options):
+            return (
+                converters[None].writer(
+                    options
+                )
+                .add_stage(ConvertPandasDataFrame(options))
+            )
+
+
+
+
+converters[pd.Series] = lambda options: (
+
+)
+
+
+    base = default_pipeline(options, )
+
+    base.add(PandaSeriesStage)
