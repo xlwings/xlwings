e3c12cdec28415ef9fc2d4dcb436872af980b5ea
Felix Zumstein <fzumstein@gmail.com>
2016-03-02 01:22:18 +0100

    refactored runtests.py to run with conda environments

--- runtests.py
+++ runtests.py
@@ -1,58 +1,69 @@
-# Run this file on Windows only. It expects full Python installations with all dependencies in the paths below.
-# TODO: change to using conda envs
-# On Mac, run the tests with tox.
-
+# This file expects a conda installation on your PATH. Run it to:
+# - build the xlwings package
+# - create conda environments that don't exist yet as specified under "envs"
+# - run the nosetests against each of these environments
 
 from __future__ import print_function
 import os
+import sys
 import inspect
-from subprocess import call
+from subprocess import check_call, check_output
 from xlwings import __version__
 
-# Python installations
-py26 = r'C:\Python26'
-py27 = r'C:\Python27'
-py31 = r'C:\Python31'  # numpy/pandas are missing for 3.1
-py32 = r'C:\Python32'
-py33 = r'C:\Python33'
-py34 = r'C:\Python34'
-
-
 class Colors:
-    # Only has an effect in the Terminal of PyCharm
     yellow = '\033[93m'
     end = '\033[0m'
 
 this_dir = os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe())))
 setup_file = os.path.abspath(os.path.join(this_dir, 'setup.py'))
 
-# Create distribution package
-call('python {0} sdist'.format(setup_file))
+# Python versions - according yml files are under tests/conda_envs
+envs = [
+    ('xw26', '2.6'),
+    ('xw27', '2.7'),
+    ('xw33', '3.3'),
+    ('xw34', '3.4'),
+    ('xw35', '3.5')
+]
 
-# Install it, run the tests and uninstall it for each Python version
-for py in [py26, py27, py31, py32, py33, py34]:
+# conda dirs
+if sys.platform.startswith('darwin'):
+    conda_dir = check_output(['which', 'conda']).decode('utf-8')
+else:
+    conda_dir = check_output(['where', 'conda']).decode('utf-8')
+envs_dir = os.path.abspath(os.path.join(os.path.dirname(conda_dir), os.pardir, 'envs'))
+
+# Create missing envs
+for env in envs:
+    if not os.path.isdir(os.path.join(envs_dir, env[0])):
+        check_call(['conda', 'env', 'create', '-f',
+                    os.path.join(this_dir, 'xlwings', 'tests', 'conda_envs', env[0] + '.yml')])
+
+# Create distribution package
+check_call(['python', setup_file, 'sdist'])
+
+# Install it and run the tests
+for py in envs:
     # Paths
-    pip = os.path.abspath(os.path.join(py, 'Scripts/pip'))
-    test_runner = os.path.abspath(os.path.join(py, 'Scripts/nosetests'))
-    test_dir = os.path.abspath(os.path.join(py, 'Lib/site-packages/xlwings/tests'))
+    if sys.platform.startswith('darwin'):
+        pip = os.path.abspath(os.path.join(envs_dir, py[0], 'bin', 'pip'))
+        test_runner = os.path.abspath(os.path.join(envs_dir, py[0], 'bin', 'nosetests'))
+        test_dir = os.path.abspath(os.path.join(envs_dir, py[0], 'lib', 'python{}'.format(py[1]), 'site-packages', 'xlwings', 'tests'))
+    else:
+        pip = os.path.abspath(os.path.join(envs_dir, py[0], 'Scripts/pip'))
+        test_runner = os.path.abspath(os.path.join(envs_dir, py[0], 'Scripts/nosetests'))
+        test_dir = os.path.abspath(os.path.join(envs_dir, py[0], 'Lib/site-packages/xlwings/tests'))
     if __version__.endswith('dev'):
         __version__ = __version__[:-3] + '.dev0'
-    xlwings_package = os.path.abspath(os.path.join(this_dir, 'dist/xlwings-{0}.zip'.format(__version__)))
+    if sys.platform.startswith('darwin'):
+        xlwings_package = os.path.abspath(os.path.join(this_dir, 'dist', 'xlwings-{0}.tar.gz'.format(__version__)))
+    else:
+        xlwings_package = os.path.abspath(os.path.join(this_dir, 'dist', 'xlwings-{0}.zip'.format(__version__)))
 
-    print('{0}### {1} ###{2}'.format(Colors.yellow, py, Colors.end))
-
-    call('{0}\python -c "import sys;print(sys.version)"'.format(py))
-    call('{0}\python -c "import numpy;import pandas;print(numpy.__version__);print(pandas.__version__)"'.format(py))
-
-    # Uninstall in case there is still an existing installation
-    call('{0} uninstall xlwings -y'.format(pip))
+    print('{0}### {1} ###{2}'.format(Colors.yellow, py[0], Colors.end))
 
     # Install
-    os.chdir(py)
-    call('{0} install {1} --ignore-installed --no-cache-dir'.format(pip, xlwings_package))
+    check_call([pip, 'install', xlwings_package, '--upgrade', '--force-reinstall', '--no-deps', '--no-cache-dir'])
 
     # Run tests
-    call('{0} {1}'.format(test_runner, test_dir))
-
-    # Uninstall
-    call('{0} uninstall xlwings -y'.format(pip))
+    check_call([test_runner, test_dir])
