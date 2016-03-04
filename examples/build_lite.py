22b18172628d8491e9b479560f9d4b2bd5580cf8
Felix Zumstein <fzumstein@gmail.com>
2016-03-04 23:34:04 +0100

    updated samples build script

--- examples/build_lite.py
+++ examples/build_lite.py
@@ -6,13 +6,12 @@ if not os.path.exists(os.path.join(this_dir, '_build')):
     os.makedirs(os.path.join(this_dir, '_build'))
 
 for this_dir, dirs, files in os.walk(this_dir):
-    if '_build' in dirs:
-        dirs.remove('_build')
     for d in dirs:
-        with ZipFile(os.path.join('_build', d + '.zip'), 'w') as zf:
-            zf.write(os.path.join(this_dir, d, 'LICENSE.txt'), 'LICENSE.txt')
-            zf.write(os.path.join(this_dir, d, d + '.py'), d + '.py')
-            zf.write(os.path.join(this_dir, d, d + '.xlsm'), d + '.xlsm')
-            if d == 'database':
-                zf.write(os.path.join(this_dir, d, 'chinook.sqlite'), 'chinook.sqlite')
+        if d not in ['build', '_build', '__pycache__']:
+            with ZipFile(os.path.join('_build', d + '.zip'), 'w') as zf:
+                zf.write(os.path.join(this_dir, d, 'LICENSE.txt'), 'LICENSE.txt')
+                zf.write(os.path.join(this_dir, d, d + '.py'), d + '.py')
+                zf.write(os.path.join(this_dir, d, d + '.xlsm'), d + '.xlsm')
+                if d == 'database':
+                    zf.write(os.path.join(this_dir, d, 'chinook.sqlite'), 'chinook.sqlite')
 
