f516847d13de293116d526e086d6c57ce61c0280
Felix Zumstein <fzumstein@gmail.com>
2016-05-24 12:09:55 +0200

    fixed Workbook.xl when doing xw.Workbook()

--- xlwings/main.py
+++ xlwings/main.py
@@ -189,7 +189,7 @@ class Workbook(xlplatform.Workbook):
                     xl = candidates[0][1].xl
             else:
                 # Open Excel if necessary and create a new workbook
-                xl = active.app.workbooks.add()
+                xl = active.app.workbooks.add().xl
 
             super(Workbook, self).__init__(xl=xl)
 
