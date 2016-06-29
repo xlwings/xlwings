daa5f9f171ea46b5b1f29f807785c1c0cec04a34
Felix Zumstein <fzumstein@gmail.com>
2016-06-29 17:06:53 +0200

    fixes #415 on mac (was missing for e.g. np.int64)

--- xlwings/_xlmac.py
+++ xlwings/_xlmac.py
@@ -910,6 +910,8 @@ def prepare_xl_data_element(x):
     elif np and isinstance(x, np.datetime64):
         # handle numpy.datetime64
         return np_datetime_to_datetime(x).replace(tzinfo=None)
+    elif np and isinstance(x, np.generic):
+        return float(x)
     elif pd and isinstance(x, pd.tslib.Timestamp):
         # This transformation seems to be only needed on Python 2.6 (?)
         return x.to_datetime().replace(tzinfo=None)
