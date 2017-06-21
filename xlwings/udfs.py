6cfee27b6bedfcc14981fe3b551654df607c671b
Colin Bounouar <Colin-b@users.noreply.github.com>
2017-06-21 19:23:35 +0200

    Allow to modify the maximum number of retry
    When multiple UDFs are depending on the same cell, when this cell is updated all those UDFs will be called by Microsoft Excel.
    It seems that Microsoft Excel will ask for a retry until all UDFs return.
    I would rather not use a value like this and retry only when all UDFs have returned instead but I did not see a way to know this as it seems that xlwings receive UDFs calls one after the other, without a link between them.
    
    So I would like to update this value from my UDFs file to tweak it properly (even if it is still an ugly solution...)

--- xlwings/udfs.py
+++ xlwings/udfs.py
@@ -171,12 +171,14 @@ udf_modules = {}
 
 
 class DelayWrite(object):
+    MAX_NUMBER_RETRY = 10
+    
     def __init__(self, rng, options, value, caller):
         self.range = rng
         self.options = options
         self.value = value
         self.skip = (caller.Rows.Count, caller.Columns.Count)
-        self.nb_remaining_call = 10
+        self.nb_remaining_call = DelayWrite.MAX_NUMBER_RETRY
 
     def __call__(self, *args, **kwargs):
         self.nb_remaining_call -= 1
