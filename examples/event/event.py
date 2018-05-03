678a8861432c8bb82baecf5cd5d7239e5f1c570f
Rafael Treviño <skasi.7+github@gmail.com>
2018-05-03 08:12:51 +0200

    Events reveive xlwings objects instead of pywin32 instances.

--- examples/event/event.py
+++ examples/event/event.py
@@ -4,22 +4,23 @@ import xlwings as xw
 
 
 @xw.event('WorkbookOpen')
-def on_workbook_open(xl_workbook):
+def on_workbook_open(book):
     win32api.MessageBox(
         xw.apps.active.api.Hwnd, 'All your workbooks are belong to us', 'Info', win32con.MB_ICONINFORMATION)
 
 
 @xw.event('SheetActivate')
-def on_sheet_activate(xl_sheet):
+def on_sheet_activate(sheet):
     win32api.MessageBox(
         xw.apps.active.api.Hwnd, 'All your sheets are belong to us', 'Info', win32con.MB_ICONINFORMATION)
 
 
 @xw.event('SheetSelectionChange')
-def on_sheet_selection_change(xl_sheet, xl_range):
-    xl_range.Value = 'X'
+def on_sheet_selection_change(sheet, rng):
+    rng.value = 'X'
 
 
 @xw.event('SheetChange')
-def on_sheet_change(xl_sheet, xl_range):
-    xl_range.Value += 1
+def on_sheet_change(sheet, rng):
+    rng.value += 1
+
