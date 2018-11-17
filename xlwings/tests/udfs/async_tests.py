027f48c31c568d9df700676683929bcd5599403b
Felix Zumstein <fzumstein@gmail.com>
2018-11-17 12:48:48 +0100

    updated tests

--- xlwings/tests/udfs/async_tests.py
+++ xlwings/tests/udfs/async_tests.py
@@ -1,20 +1,55 @@
 import xlwings as xw
 import time
+import numpy as np
+import pandas as pd
 
 
-@xw.func(async='threading')
-def async1(a):
-    time.sleep(2)
-    return a
-
-
-@xw.func(async='threading')
+@xw.func(async_mode='threading')
 @xw.arg("n", numbers=int)
 @xw.arg("m", numbers=int)
 @xw.ret(expand='table')
-def async2(n, m):
-    time.sleep(2)
+def threading(n, m):
+    print('CALLED THREADING FUNCTION')
+    time.sleep(1)
     return [
         ["%s x %s : %s, %s" % (n, m, i, j) for j in range(m)]
         for i in range(n)
     ]
+
+
+@xw.arg("n", numbers=int)
+@xw.arg("m", numbers=int)
+@xw.ret(expand='table')
+def nothreading(n, m):
+    print('CALLED NOTHREADING FUNCTION')
+    return [
+        ["%s x %s : %s, %s" % (n, m, i, j) for j in range(m)]
+        for i in range(n)
+    ]
+
+
+@xw.func
+def simple(x):
+    print('CALLED SIMPLE FUNCTION')
+    return x
+
+
+@xw.func(async_mode='threading')
+def simple_threading(x):
+    print('CALLED SIMPLE THREADING FUNCTION')
+    time.sleep(1)
+    return x
+
+
+@xw.func(async_mode='threading')
+@xw.arg("i", numbers=int)
+@xw.arg("j", numbers=int)
+@xw.ret(expand='table')
+def numpy_async(i, j):
+    print('CALLED NUMPY ASYNC FUNCTION')
+    time.sleep(1)
+    return np.arange(i * j).reshape((i, j))
+
+
+if __name__ == '__main__':
+    xw.serve()
