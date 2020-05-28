be1cb650bae0fc5598f388055be33a3285e8fcc4
Felix Zumstein <fzumstein@gmail.com>
2020-05-28 23:47:54 +0200

    added import test to CI (#1326)
    * bump python actions
    * added simple package import to test build

---
+++ scripts/ci_test.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+from shlex import split
+
+# Version string
+if os.environ['GITHUB_REF'].startswith('refs/tags'):
+    version_string = os.environ['GITHUB_REF'][10:]
+else:
+    version_string = os.environ['GITHUB_SHA'][:7]
+
+# Installation
+subprocess.check_call(split(f'python -m pip install Package/xlwings-{version_string}.tar.gz'))
+
+# Changing the dir is required to prevent python from importing the package from the source code
+os.chdir(os.path.expanduser('~'))  # e.g. /Users/runners
+output = subprocess.check_output(split('python -c "import xlwings;print(xlwings.__version__)"'),
+                                 stderr=subprocess.STDOUT).decode()
+print('Version: ' + output)
+
+
