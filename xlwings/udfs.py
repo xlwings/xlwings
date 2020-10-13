c37520ae162477f7ded2f05c40634fa566ba4ca5
Felix Zumstein <fzumstein@gmail.com>
2020-10-13 16:22:38 +0200

    moved the ComRange wrapper

--- xlwings/udfs.py
+++ xlwings/udfs.py
@@ -423,13 +423,13 @@ def call_udf(module_name, func_name, args, this_workbook=None, caller=None):
     args_info = func_info['args']
     ret_info = func_info['ret']
     is_dynamic_array = ret_info['options'].get('expand')
-    xw_caller = ComRange(Range(impl=xlplatform.Range(xl=caller)))
+    xw_caller = Range(impl=xlplatform.Range(xl=caller))
 
     # If there is the 'reserved' argument "caller", assign the caller object
     for info in args_info:
         if info['name'] == 'caller':
             args = list(args)
-            args[info['pos']] = xw_caller
+            args[info['pos']] = ComRange(xw_caller)
             args = tuple(args)
 
     writing = func_info.get('writing', None)
