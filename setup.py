49a76eca0257479fd6aae049dfc01918dfb3d2a0
Felix Zumstein <fzumstein@gmail.com>
2020-11-12 09:52:46 +0100

    added Python 3.9 in descriptors [skip ci]

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Office/Business :: Financial :: Spreadsheet',
         'License :: OSI Approved :: BSD License'],
     platforms=['Windows', 'Mac OS X'],
