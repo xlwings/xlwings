c9f4c2b32247e3291e5260f1b9f552bc373a79bb
Felix Zumstein <fzumstein@gmail.com>
2021-02-04 15:26:19 +0100

    don't delete unnecessary rows/cols in mytable.udpate()

--- xlwings/pro/tables.py
+++ xlwings/pro/tables.py
@@ -9,11 +9,13 @@ def update(self, data):
     if pd:
         if not isinstance(data, pd.DataFrame):
             raise TypeError(type_error_msg)
-        col_diff = len(self.range.columns) - len(data.columns)
+        col_diff = len(self.range.columns) - len(data.columns) - len(data.index.names)
+        nrows = len(self.data_body_range.rows) if self.data_body_range else 1
+        row_diff = nrows - len(data.index)
         if col_diff > 0:
             self.range[:, len(self.range.columns) - col_diff:].delete()
-        if self.data_body_range:
-            self.data_body_range.delete()
+        if row_diff > 0 and self.data_body_range:
+            self.data_body_range[len(self.data_body_range.rows) - row_diff:, :].delete()
         self.header_row_range.value = list(data.index.names) + list(data.columns)
         self.range[1:, :].options(index=True, header=False).value = data
         return self
