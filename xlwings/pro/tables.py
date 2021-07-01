63c7dec78325adefd6c7f545930144d865dafd90
Felix Zumstein <fzumstein@gmail.com>
2021-07-01 17:49:23 +0200

    table.update fixes

--- xlwings/pro/tables.py
+++ xlwings/pro/tables.py
@@ -9,7 +9,7 @@ def update(self, data, index):
     if pd:
         if not isinstance(data, pd.DataFrame):
             raise TypeError(type_error_msg)
-        col_diff = len(self.range.columns) - len(data.columns) - len(data.index.names) if index else 0
+        col_diff = len(self.range.columns) - len(data.columns) - (len(data.index.names) if index else 0)
         nrows = len(self.data_body_range.rows) if self.data_body_range else 1
         row_diff = nrows - len(data.index)
         if col_diff > 0:
@@ -18,14 +18,13 @@ def update(self, data, index):
             self.data_body_range[len(self.data_body_range.rows) - row_diff:, :].delete()
         if self.header_row_range:
             # You can uncheck the header row in an Excel table
-            self.header_row_range.value = list(data.index.names) + list(data.columns) if index else list(data.columns)
+            self.header_row_range.value = (list(data.index.names) + list(data.columns)) if index else list(data.columns)
             self.range[1:, :].options(index=index, header=False).value = data
         else:
             # Without a table header, the table is deleted...
             self.show_headers = True
             self.range[1:, :].options(index=index, header=False).value = data
             self.show_headers = False
-            self.range.offset(row_offset=-1).resize(row_size=1).delete(shift='up')
         return self
     else:
         raise TypeError(type_error_msg)
