19297c16691511c46e8e5b2721288b362d9b437a
Felix Zumstein <fzumstein@gmail.com>
2021-10-26 18:09:20 +0200

    added 3.10 as a supported language

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Office/Business :: Financial :: Spreadsheet',
         'License :: OSI Approved :: BSD License'],
     platforms=['Windows', 'Mac OS X'],
