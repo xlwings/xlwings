9993c4d4a22c9d80b20eaf9bbdd441759cbae69d
Felix Zumstein <fzumstein@gmail.com>
2022-06-08 16:39:11 +0200

    better handling of embedded code, making sure that the exported code in the tempfile doesn't get GC too early

--- xlwings/pro/embedded_code.py
+++ xlwings/pro/embedded_code.py
@@ -12,20 +12,21 @@ Commercial licenses can be purchased at https://www.xlwings.org
 """
 
 import os
-import glob
-import time
-import shutil
 import sys
-import tempfile
+from functools import lru_cache
 
 from .utils import LicenseHandler
 from .module_permissions import verify_execute_permission
 from ..main import Book
+from .utils import get_embedded_code_temp_dir
 from ..utils import get_cached_user_config
 
 LicenseHandler.validate_license("pro")
 
+TEMPDIR = get_embedded_code_temp_dir()
 
+
+@lru_cache()
 def dump_embedded_code(book, target_dir):
     for sheet in book.sheets:
         if sheet.name.endswith(".py"):
@@ -49,26 +50,10 @@ def dump_embedded_code(book, target_dir):
 
 
 def runpython_embedded_code(command):
-    with tempfile.TemporaryDirectory(prefix="xlwings-") as tempdir:
-        dump_embedded_code(Book.caller(), tempdir)
-        if (
-            get_cached_user_config("permission_check_enabled")
-            and get_cached_user_config("permission_check_enabled").lower() == "true"
-        ):
-            verify_execute_permission(command=command)
-        exec(command)
-
-
-def get_udf_temp_dir():
-    tmp_base_path = os.path.join(tempfile.gettempdir(), "xlwingsudfs")
-    os.makedirs(tmp_base_path, exist_ok=True)
-    try:
-        # HACK: Clean up directories that are older than 30 days
-        # This should be done in the C++ part when the Python process is killed
-        for subdir in glob.glob(tmp_base_path + "/*/"):
-            if os.path.getmtime(subdir) < time.time() - 30 * 86400:
-                shutil.rmtree(subdir, ignore_errors=True)
-    except Exception:
-        pass  # we don't care if it fails
-    tempdir = tempfile.TemporaryDirectory(dir=tmp_base_path)
-    return tempdir
+    dump_embedded_code(Book.caller(), TEMPDIR)
+    if (
+        get_cached_user_config("permission_check_enabled")
+        and get_cached_user_config("permission_check_enabled").lower() == "true"
+    ):
+        verify_execute_permission(command=command)
+    exec(command)
