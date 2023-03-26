d93e116fdf9dd047b66bda4b8af2df7c660ce996
Felix Zumstein <fzumstein@gmail.com>
2023-03-26 22:32:31 +0200

    fix pipeline (#2206)

--- xlwingsjs/check_version.py
+++ xlwingsjs/check_version.py
@@ -6,16 +6,23 @@ import os
 from pathlib import Path
 
 this_dir = Path(__file__).resolve().parent
-if os.environ["GITHUB_REF"].startswith("refs/tags"):
-    version = os.environ["GITHUB_REF"][10:]
-else:
-    raise Exception("Didn't find Git tag!")
 
-xlwingsjs = this_dir / "dist" / "xlwings.js"
-xlwingsminjs = this_dir / "dist" / "xlwings.min.js"
 
-if f'version = "{version}"' not in xlwingsjs.read_text():
-    raise Exception("Didn't find expected version in xlwings.js!")
+def main():
+    if os.environ["GITHUB_REF"].startswith("refs/tags"):
+        version = os.environ["GITHUB_REF"][10:]
+    else:
+        return
 
-if f'b.version="{version}"' not in xlwingsminjs.read_text():
-    raise Exception("Didn't find expected version in xlwings.min.js!")
+    xlwingsjs = this_dir / "dist" / "xlwings.js"
+    xlwingsminjs = this_dir / "dist" / "xlwings.min.js"
+
+    if f'version = "{version}"' not in xlwingsjs.read_text():
+        raise Exception("Didn't find expected version in xlwings.js!")
+
+    if f'version="{version}"' not in xlwingsminjs.read_text():
+        raise Exception("Didn't find expected version in xlwings.min.js!")
+
+
+if __name__ == "__main__":
+    main()
