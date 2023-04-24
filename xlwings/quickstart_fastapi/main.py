8836c1de66314b25cb5d7a9c7c5176bd5d7df3bb
Felix Zumstein <fzumstein@gmail.com>
2023-04-24 14:57:26 +0200

    handle cors with all exceptions in fastapi quickstart

--- xlwings/quickstart_fastapi/main.py
+++ xlwings/quickstart_fastapi/main.py
@@ -1,5 +1,6 @@
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import PlainTextResponse
 
 import xlwings as xw
 
@@ -23,16 +24,25 @@ def hello(data: dict = Body):
     return book.json()
 
 
-# Excel via OfficeScripts requires CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origin_regex=r"https://.*.officescripts.microsoftusercontent.com",
+@app.exception_handler(Exception)
+async def exception_handler(request, exception):
+    # This handles all exceptions, so you may want to make this more restrictive
+    return PlainTextResponse(
+        str(exception), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+    )
+
+
+# Office Scripts and custom functions in Excel on the web require CORS
+cors_app = CORSMiddleware(
+    app=app,
+    allow_origins="*",
     allow_methods=["POST"],
     allow_headers=["*"],
+    allow_credentials=True,
 )
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:cors_app", host="127.0.0.1", port=8000, reload=True)
