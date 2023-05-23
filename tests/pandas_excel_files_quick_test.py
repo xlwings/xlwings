689177235d6f6d2827fc39858fc3154a1a18cb4d
Felix Zumstein <fzumstein@gmail.com>
2023-05-23 15:48:51 +0200

    updated quick test

--- tests/pandas_excel_files_quick_test.py
+++ tests/pandas_excel_files_quick_test.py
@@ -22,12 +22,19 @@ path = (
 
 ix = 0
 for ix, f in enumerate(path.glob("[!~$]*.xls*")):
-    print(f.resolve())
-    with xw.Book(f.resolve(), mode="r") as book:
-        for sheet in book.sheets:
-            print(sheet.name)
-            data = sheet.cells.value
-            print(data)
+    file_name = f.resolve()
+    if "testdateoverflow" in str(file_name):
+        # 1e+20 formatted as date isn't supported anymore since
+        # https://github.com/tafia/calamine/pull/303
+        continue
+    try:
+        with xw.Book(f.resolve(), mode="r") as book:
+            for sheet in book.sheets:
+                sheet_name = sheet.name
+                data = sheet.cells.value
+        print(".", end="", flush=True)
+    except:  # noqa: E722
+        print(f"FAILED: {f.resolve()} [repr(e)]")
 
 print()
 print(f"Tested {ix + 1} files.")
