05eb5f9544e1c294866a5ecb977b42513ec881a4
Felix Zumstein <fzumstein@gmail.com>
2023-06-22 08:18:37 +0200

    removed 3.7 from setup.py

--- setup.py
+++ setup.py
@@ -121,7 +121,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -129,5 +128,5 @@ setup(
         "Topic :: Office/Business :: Financial :: Spreadsheet",
         "License :: OSI Approved :: BSD License",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
