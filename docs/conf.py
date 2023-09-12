095ac50f8797321cb3a8b8ce7c93889d7f70aff4
Felix Zumstein <fzumstein@gmail.com>
2023-09-12 09:23:52 +0200

    announcement

--- docs/conf.py
+++ docs/conf.py
@@ -195,7 +195,7 @@ html_theme_options = {
         "color-brand-primary": "white",
         "color-announcement-background": "#28a745",
     },
-    "announcement": 'Webinar (Sep 15, 2023): Excel Custom Functions with Python and Azure Functions: <a href="https://zoomeranalytics.webinargeek.com/excel-custom-functions-with-python-and-azure-functions-1?cst=docs">Sign up!</a>',
+    "announcement": 'Webinar (Sep 14, 2023): Excel Custom Functions with Python and Azure Functions: <a href="https://zoomeranalytics.webinargeek.com/excel-custom-functions-with-python-and-azure-functions-1?cst=docs">Sign up!</a>',
 }
 
 html_show_sourcelink = False
