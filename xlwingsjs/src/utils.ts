e16af44c56cfc862d2e90d37bf0df441fb831b43
Felix Zumstein <fzumstein@gmail.com>
2024-03-31 22:18:25 +0200

    await Office.onReady() in all js functions

--- xlwingsjs/src/utils.ts
+++ xlwingsjs/src/utils.ts
@@ -1,5 +1,6 @@
 export async function getActiveBookName() {
   try {
+    await Office.onReady();
     return await Excel.run(async (context) => {
       const workbook = context.workbook;
       workbook.load("name");
