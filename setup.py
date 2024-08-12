033894991d25c8d7efa7b8deefa72d5fedfc6d69
Felix Zumstein <fzumstein@gmail.com>
2024-08-12 22:40:15 +0200

    drop python 3.8

--- setup.py
+++ setup.py
@@ -124,7 +124,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -132,5 +131,5 @@ setup(
         "Topic :: Office/Business :: Financial :: Spreadsheet",
         "License :: OSI Approved :: BSD License",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
 )
